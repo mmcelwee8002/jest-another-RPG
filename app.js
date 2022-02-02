3c0da2b583807dec443f669f18e71db0c0262459
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-02-02 13:21:38 -0600

    game logic

--- app.js
+++ app.js
@@ -1,3 +1,59 @@
 const Game = require('./lib/Game');
-
+const inquirer = require('inquirer');
 new Game().initializeGame();
+
+Game.prototype.battle = function () {
+    if (this.isPlayerTurn) {
+
+        // player prompts will go here
+    } else {
+        const damage = this.currentEnemy.getAttackValue();
+        this.player.reduceHealth(damage);
+
+        console.log(`You were attacked by the ${this.currentEnemy.name}`);
+        console.log(this.player.getHealth());
+    }
+    if (this.isPlayerTurn) {
+        inquirer
+            .prompt({
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'action',
+                choices: ['Attack', 'Use potion']
+            })
+            .then(({ action }) => {
+                if (action === 'Use potion') {
+                    // follow-up prompt will go here
+                } else {
+                    const damage = this.player.getAttackValue();
+                    this.currentEnemy.reduceHealth(damage);
+
+                    console.log(`You attacked the ${this.currentEnemy.name}`);
+                    console.log(this.currentEnemy.getHealth());
+                }
+                if (action === 'Use potion') {
+                    if (!this.player.getInventory()) {
+                        console.log("You don't have any potions!");
+                        return;
+                    }
+
+                    inquirer
+                        .prompt({
+                            type: 'list',
+                            message: 'Which potion would you like to use?',
+                            name: 'action',
+                            choices: this.player.getInventory().map((item, index) => `${index + 1}: ${item.name}`)
+                        })
+                        .then(({ action }) => {
+                            const potionDetails = action.split(': ');
+
+                            this.player.usePotion(potionDetails[0] - 1);
+                            console.log(`You used a ${potionDetails[1]} potion.`);
+                        });
+                }
+
+
+            });
+    }
+
+};
